3150530d88b6b5c3bb6134f01562e840efedbc37
Djordje Knezevic <starwarrior@mycity.rs>
2022-01-08 12:41:06 +0100

    Add logic for sending POST request to store new meetup data to firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,21 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+    function addMeetupHandler(meetupData) {
+        fetch('https://react-getting-started-9971c-default-rtdb.europe-west1.firebasedatabase.app/meetups.json', 
+        {
+           method: 'POST',
+           body: JSON.stringify(meetupData),
+           headers: {
+               'Content-Type': 'application/json',
+           },
+        }
+      );
+    };
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
         </section>
     );
 };
